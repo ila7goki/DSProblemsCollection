5f546d95b0e4300b70d8ca7e5307db40ff14cf80
Gokila Rajaiah <grajaiah2010@my.fit.edu>
2017-11-03 07:13:37 -0700

    Implemented Graph, AddEdge and DFS

--- src/Graphs/Graph.java
+++ src/Graphs/Graph.java
@@ -1,17 +1,81 @@
 package Graphs;
 
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Created by Goki_PC on 10/31/2017.
  */
 public class Graph {
-    public LinkedList<Integer> adjacentNodes[];
-    public int vertices;
 
-    public Graph(int vertices)
+    private HashMap<Integer, GraphNode> lookupMap = new HashMap<>();
+
+    public GraphNode getNode(int id)
     {
-        this.vertices = vertices;
-        adjacentNodes = new LinkedList[vertices];
+        return this.lookupMap.get(id);
     }
+
+    public void addEdge(int src, int dest) {
+        GraphNode s, d;
+        if (!this.lookupMap.containsKey(src)) {
+            s = new GraphNode(src);
+            this.lookupMap.put(src, s);
+        } else {
+            s = this.lookupMap.get(src);
+        }
+
+        if (!this.lookupMap.containsKey(dest)) {
+            d = new GraphNode(dest);
+            this.lookupMap.put(dest, d);
+        } else {
+            d = this.lookupMap.get(dest);
+        }
+
+        s.adjacentNodes.addFirst(d);
+    }
+
+
+    public void DFS(Graph graph) {
+        HashSet<Integer> visited = new HashSet<>();
+
+        for (GraphNode gn : graph.lookupMap.values()) {
+            if (!visited.contains(gn.getId())) {
+                DFSUtil(gn, visited);
+            }
+        }
+    }
+
+    private void DFSUtil(GraphNode graphNode, HashSet<Integer> visited) {
+        visited.add(graphNode.getId());
+        System.out.println(graphNode.getId());
+
+        //if(visited.contains(graphNode.getId())) return false;
+        for (GraphNode aN : graphNode.grAdjacentNodeList()) {
+            if (!visited.contains(aN.getId())) {
+                DFSUtil(aN, visited);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 7);
+        g.addEdge(2, 6);
+        g.addEdge(2, 3);
+        g.addEdge(3, 5);
+        g.addEdge(4, 6);
+        g.addEdge(4, 5);
+        g.addEdge(4, 3);
+
+        for (GraphNode gn : g.lookupMap.values()) {
+            System.out.println(gn.id + " -> " + gn.getAdjacentNodesStr());
+        }
+
+        System.out.println("Depth First Traversal: ");
+        g.DFS(g);
+    }
+
 }
--- src/Graphs/GraphNode.java
+++ src/Graphs/GraphNode.java
@@ -1,9 +1,37 @@
 package Graphs;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Goki_PC on 10/31/2017.
  */
 public class GraphNode {
-    public int name;
-    public GraphNode[] adjacentNodes;
+    public int id;
+    public LinkedList<GraphNode> adjacentNodes;
+
+    public GraphNode(int id) {
+        this.id = id;
+        adjacentNodes = new LinkedList<>();
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public List<GraphNode> grAdjacentNodeList() {
+        return this.adjacentNodes;
+    }
+
+    public String getAdjacentNodesStr() {
+
+        StringBuilder adjacentNodeListStr = new StringBuilder();
+        for (int i = 0; i < adjacentNodes.size(); i++) {
+            adjacentNodeListStr.append(adjacentNodes.get(i).id);
+            if (i < adjacentNodes.size() - 1) {
+                adjacentNodeListStr.append(", ");
+            }
+        }
+        return adjacentNodeListStr.toString();
+    }
 }
