016ec6fadacda7cfbc6a3dba5cf84c14b4996b72
Gokila Rajaiah <grajaiah2010@my.fit.edu>
2017-11-03 08:59:46 -0700

    Implemented BFS

--- src/Graphs/Graph.java
+++ src/Graphs/Graph.java
@@ -1,7 +1,6 @@
 package Graphs;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * Created by Goki_PC on 10/31/2017.
@@ -10,9 +9,43 @@ public class Graph {
 
     private HashMap<Integer, GraphNode> lookupMap = new HashMap<>();
 
-    public GraphNode getNode(int id)
-    {
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 7);
+        g.addEdge(2, 6);
+        g.addEdge(2, 3);
+        g.addEdge(3, 5);
+        g.addEdge(4, 6);
+        g.addEdge(4, 5);
+        g.addEdge(4, 3);
+
+        for (GraphNode gn : g.lookupMap.values()) {
+            System.out.println(gn.id + " -> " + gn.getAdjacentNodesStr());
+        }
+
+        System.out.println("Depth First Traversal: ");
+        g.DFS(g);
+
+        System.out.println("Breadth First Traversal: ");
+        g.BFS(g);
+    }
+
+    public List<GraphNode> getAllGraphNodes() {
+        return new LinkedList<>(lookupMap.values());
+        //return lookupMap.values().stream().collect(Collectors.toList()); // Java 8 feature
+    }
+
+    public GraphNode getNode(int id) {
         return this.lookupMap.get(id);
+    }
+
+    public GraphNode getFirstNode() {
+        if (!this.lookupMap.isEmpty())
+            return this.lookupMap.entrySet().iterator().next().getValue();
+        else
+            return null;
     }
 
     public void addEdge(int src, int dest) {
@@ -34,6 +67,8 @@ public class Graph {
         s.adjacentNodes.addFirst(d);
     }
 
+    /*** Traversal of Graph using Depth first approach. i.e. we look at deep into a child node before looking at other
+     *   children/adjacent ndoes. ***/
 
     public void DFS(Graph graph) {
         HashSet<Integer> visited = new HashSet<>();
@@ -57,25 +92,31 @@ public class Graph {
         }
     }
 
+    /*** Traversal of the Graph using Breadth first approach. i.e. All nodes in a level are visited first
+     *   before their child nodes. ***/
 
-    public static void main(String[] args) {
-        Graph g = new Graph();
-        g.addEdge(1, 2);
-        g.addEdge(1, 3);
-        g.addEdge(1, 7);
-        g.addEdge(2, 6);
-        g.addEdge(2, 3);
-        g.addEdge(3, 5);
-        g.addEdge(4, 6);
-        g.addEdge(4, 5);
-        g.addEdge(4, 3);
+    public void BFS(Graph g) {
+        HashSet<Integer> visited = new HashSet<>();
+        Queue<GraphNode> nextToVisit = new LinkedList<>();
 
-        for (GraphNode gn : g.lookupMap.values()) {
-            System.out.println(gn.id + " -> " + gn.getAdjacentNodesStr());
+        for (GraphNode node : g.getAllGraphNodes()) {
+
+            if (!visited.contains(node.getId())) {
+                nextToVisit.add(node);
+
+                while (!nextToVisit.isEmpty()) {
+                    GraphNode currNode = nextToVisit.remove();
+
+                    if (visited.contains(currNode.getId())) continue;
+
+                    visited.add(currNode.getId());
+                    System.out.println(currNode.getId());
+
+                    for (GraphNode gn : currNode.grAdjacentNodeList()) {
+                        nextToVisit.add(gn);
+                    }
+                }
+            }
         }
-
-        System.out.println("Depth First Traversal: ");
-        g.DFS(g);
     }
-
 }
