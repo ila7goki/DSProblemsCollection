e1b3ddd4b7eb92c795aaaf34ceac8137ff57e25d
Gokila Rajaiah <grajaiah2010@my.fit.edu>
2017-11-09 08:23:04 -0800

    Matrix - Rotate 90 degrees

--- src/StringsAndArrays/MatrixManipulations.java
+++ src/StringsAndArrays/MatrixManipulations.java
@@ -5,7 +5,7 @@ package StringsAndArrays;
  */
 public class MatrixManipulations {
 
-    public void rotate90Degrees(int[][] input)
+    public static void rotate90DegreesClockwise(int[][] input)
     {
         int n = input.length;
         int layer = n/2;
@@ -16,19 +16,38 @@ public class MatrixManipulations {
             {
                 int temp = input[i][j];
 
-                input[i][j] = input[j][n-layer];
-
-                //input[j][n-layer] = input[n-layer][]
-
+                input[i][j] = input[n-1-j][i];
+                input[n-1-j][i] = input[n-1-i][n-1-j];
+                input[n-1-i][n-1-j] = input[j][n-1-i];
+                input[j][n-1-i] = temp;
             }
         }
+    }
 
+    static void displayMatrix(int mat[][])
+    {
+        for (int i = 0; i < mat.length; i++)
+        {
+            for (int j = 0; j < mat.length; j++)
+                System.out.print(" " + mat[i][j]);
 
+            System.out.print("\n");
+        }
+        System.out.print("\n");
     }
 
 
     public static void main(String[] args)
     {
+        int mat[][] =
+                {
+                        {1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}
+                };
 
+        rotate90DegreesClockwise(mat);
+        displayMatrix(mat);
     }
 }
